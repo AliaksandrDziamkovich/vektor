eb6e8acd350103f7466fba0fddc49d442d419e2d
AliaksandrDziamkovich <34609419+AliaksandrDziamkovich@users.noreply.github.com>
2018-01-21 11:59:26 +0300

    Initial commit

---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class VektorDemo
+    {
+        public static void Main()
+        {
+            Vektor a = new Vektor(1, 2);
+            Vektor b = new Vektor(10, 10);
+            Vektor c = new Vektor();
+            c = a + b;
+            c.show();
+            Console.ReadKey();
+        }
+    } 
+}
---
+++ ConsoleApp29/Vektor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Vektor
+    {
+        int x, y;
+
+        public Vektor()
+        {
+            x = y = 0;
+        }
+        public Vektor(int i, int j)
+        {
+            x = i;
+            y = j;
+        }
+
+        public static Vektor operator +(Vektor op1, Vektor op2)
+        {
+            Vektor result = new Vektor();
+            result.x = op1.x + op2.x;
+            result.y = op1.y + op2.y;
+            return result;
+        }
+
+        public static Vektor operator -(Vektor op1, Vektor op2)
+        {
+            Vektor result = new Vektor();
+            result.x = op1.x - op2.x;
+            result.y = op1.y - op2.y;
+            return result;
+        }
+        public static Vektor operator *(Vektor op1, Vektor op2)
+        {
+            Vektor result = new Vektor();
+            result.x = op1.x * op2.x;
+            result.y = op1.y * op2.y;
+            return result;
+        }
+
+        public void show()
+        {
+            Console.WriteLine(x + ", " + y);
+        }
+    }
+}
